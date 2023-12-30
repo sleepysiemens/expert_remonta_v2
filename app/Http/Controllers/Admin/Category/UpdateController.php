4fc4722c8a8e2be07f5c851b8e0c2ef5fd03efcd
sleepysiemens <2001sema@gmail.com>
2023-12-30 07:09:42 +0700

    fix2

--- app/Http/Controllers/Admin/Category/UpdateController.php
+++ app/Http/Controllers/Admin/Category/UpdateController.php
@@ -19,7 +19,7 @@ class UpdateController extends Controller
             $sql_data=['title_ru'=>request()->title_ru, 'title_kz'=>request()->title_kz, 'url'=>request()->url, 'description_ru'=>htmlspecialchars(request()->description_ru, ENT_QUOTES), 'description_kz'=>htmlspecialchars(request()->description_kz, ENT_QUOTES), 'src'=>(request()->title).'-image.img', 'service_id'=>request()->service_id];
         }
         else
-            $sql_data=['title'=>request()->title, 'url'=>request()->url, 'description'=>request()->description, 'service_id'=>request()->service_id];
+            $sql_data=['title_ru'=>request()->title_ru, 'title_kz'=>request()->title_kz, 'url'=>request()->url, 'description_ru'=>htmlspecialchars(request()->description_ru, ENT_QUOTES), 'description_kz'=>htmlspecialchars(request()->description_kz, ENT_QUOTES), 'service_id'=>request()->service_id];
 
         $category->update($sql_data);
 
