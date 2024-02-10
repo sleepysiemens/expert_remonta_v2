e5c5b299acb2e469762abf9eeb4a2ed8689b81e7
Your Name <you@example.com>
2024-02-10 09:46:14 +0000

    different users

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,6 +12,10 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+    public function getTable()  {
+        return env('APP_CITY') === 'Астана' ? 'users' : 'users_almaty';
+    }
+
     /**
      * The attributes that are mass assignable.
      *
